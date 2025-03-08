a63d27504e83346a0b90bc6734a492f5889768d3
kanhaiya kumar <118474316+kanhaiyakum@users.noreply.github.com>
2025-03-08 23:22:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (94.84%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,11 +1,26 @@
+// class Solution {
+//     public boolean checkPerfectNumber(int num) {
+//         int sum =0;
+//         for(int i=1;i<=num/2;i++){
+//             if(num%i==0){
+//                 sum+=i;
+//             }
+//         }if(sum==num)return true;
+//         else return false;
+//     }
+// }
+
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum =0;
-        for(int i=1;i<=num/2;i++){
-            if(num%i==0){
-                sum+=i;
-            }
-        }if(sum==num)return true;
-        else return false;
+        switch (num) {
+            case 6:
+            case 28:
+            case 496:
+            case 8128:
+            case 33550336:
+                return true;
+            default:
+                return false;
+        }
     }
 }

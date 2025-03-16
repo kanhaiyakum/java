9402cb710e7e3e7ac8159cab01466e37c94b14f5
kanhaiya kumar <118474316+kanhaiyakum@users.noreply.github.com>
2025-03-16 22:22:25 +0530

    Time: 35 ms (72.58%) | Memory: 56.6 MB (32.53%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.java
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.java
@@ -1,24 +1,49 @@
 class Solution {
-    public boolean timeIsSuff(int[] ranks, int cars, long minGiven) {
-        long carsDone = 0;
-        for (int r : ranks) {
-            long c2 = minGiven / r;
-            long c = (long) Math.sqrt(c2);
-            carsDone += c;
+    public long repairCars(int[] ranks, int cars) {
+        int maxRank=Integer.MIN_VALUE;
+        for(int i=0;i<ranks.length;i++)
+        {
+            maxRank=Math.max(maxRank,ranks[i]);
         }
-        return carsDone >= cars;
+        long max = (long) maxRank * cars * cars;
+
+        return bs(1,max,ranks,cars);
     }
 
-    public long repairCars(int[] ranks, int cars) {
-        long l = 1, r = (long) 1e14;
-        while (l < r) {
-            long mid = (l + r) / 2;
-            if (timeIsSuff(ranks, cars, mid)) {
-                r = mid;
-            } else {
-                l = mid + 1;
-            }
+    public long bs(long l, long r, int[] ranks, int cars)
+    {
+        
+        long mid=(l+r)/2;
+
+        if(l>=r)
+        {
+            return mid;
         }
-        return l;
+        
+        if(valid(mid,ranks,cars)==true)
+        {
+            return bs(l,mid,ranks,cars);
+        }
+        else
+        {
+            return bs(mid+1,r,ranks,cars);
+        }
+    }
+
+    public boolean valid(long T,int[] ranks, int cars)
+    {
+        long sum=0;
+        for(int i=0;i<ranks.length;i++)
+        {
+            sum=sum+(long)Math.sqrt(T/ranks[i]);
+        }
+        if(sum>=cars)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }

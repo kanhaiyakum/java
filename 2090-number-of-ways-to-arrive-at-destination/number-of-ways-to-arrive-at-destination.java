9d97c112aafe061d0ee2c92c5226cfb00dc221db
kanhaiya kumar <118474316+kanhaiyakum@users.noreply.github.com>
2025-03-23 15:01:39 +0530

    Time: 14 ms (31.70%) | Memory: 49 MB (78.10%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -1,45 +1,43 @@
-import java.util.*;
-
 class Solution {
     public int countPaths(int n, int[][] roads) {
-        int mod = 1_000_000_007;
-        List<int[]>[] adj = new ArrayList[n];
-        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
-
-        for (int[] r : roads) {
-            adj[r[0]].add(new int[]{r[1], r[2]});
-            adj[r[1]].add(new int[]{r[0], r[2]});
+        List<List<int[]>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++){
+            graph.add(new ArrayList<>());
         }
-
-        long[] shortesttime = new long[n];
-        int[] cnt = new int[n];
-        Arrays.fill(shortesttime, Long.MAX_VALUE);
+        for (int[] road : roads){
+            int u = road[0], v = road[1], time = road[2];
+            graph.get(u).add(new int[]{v, time});
+            graph.get(v).add(new int[]{u, time});
+        }
+        final int MOD = 1_000_000_007;
+        long[] distance = new long[n];
+        long[] count = new long[n];
+        Arrays.fill(distance, Long.MAX_VALUE);
+        distance[0] = 0;
+        count[0] = 1;
 
         PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(a -> a[0]));
-        shortesttime[0] = 0;
-        cnt[0] = 1;
         pq.offer(new long[]{0, 0});
-
-        while (!pq.isEmpty()) {
-            long[] top = pq.poll();
-            long time = top[0];
-            int node = (int) top[1];
-
-            if (time > shortesttime[node]) continue;
-
-            for (int[] edge : adj[node]) {
-                int nbr = edge[0], rtime = edge[1];
-
-                if (time + rtime < shortesttime[nbr]) {
-                    shortesttime[nbr] = time + rtime;
-                    cnt[nbr] = cnt[node];
-                    pq.offer(new long[]{shortesttime[nbr], nbr});
-                } else if (time + rtime == shortesttime[nbr]) {
-                    cnt[nbr] = (cnt[nbr] + cnt[node]) % mod;
+        while (!pq.isEmpty()){
+            long[] curr = pq.poll();
+            long curr_dist = curr[0];
+            int u = (int) curr[1];
+            if (curr_dist > distance[u]){
+                continue;
+            }
+            for (int[] nei : graph.get(u)){
+                int v = nei[0], time = nei[1];
+                long new_path = curr_dist + time;
+                if (new_path < distance[v]){
+                    distance[v] = new_path;
+                    count[v] = count[u];
+                    pq.add(new long[]{new_path, v});
+                }
+                else if (new_path == distance[v]){
+                    count[v] = (count[v] + count[u]) % MOD;
                 }
             }
         }
-
-        return cnt[n - 1];
+        return (int) count[n - 1];
     }
 }
